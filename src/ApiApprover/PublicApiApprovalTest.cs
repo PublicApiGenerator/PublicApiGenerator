ba477385253894fa96f07135e685c7dbd9356d1b
Jake Ginnivan <jake.ginnivan@readify.net>
2012-02-19 17:28:23 +0800

    Initial release of ApiApprover

---
+++ src/ApiApprover/PublicApiApprovalTest.cs
@@ -0,0 +1,24 @@
+﻿using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+
+namespace ApiApprover
+{
+    public class PublicApiApprovalTest
+    {
+        [Test]
+        public void approve_public_api()
+        {
+            // arrange
+            var assembly = typeof(SOMETYPE).Assembly;
+
+            // act
+            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(assembly);
+
+            // assert
+            var unitTestFrameworkNamer = new UnitTestFrameworkNamer();
+            var diffReporter = new DiffReporter();
+            Approvals.Approve(new ApprovalTextWriter(publicApi), unitTestFrameworkNamer, diffReporter);
+        }
+    }
+}
