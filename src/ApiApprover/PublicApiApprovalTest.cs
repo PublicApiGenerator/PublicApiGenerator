f9685e0156507834192298ac39606881faaa8bc0
Matt Ellis <m.t.ellis@gmail.com>
2013-10-29 12:04:40 +0000

    Make it easy to use row tests

--- src/ApiApprover/PublicApiApprovalTest.cs
+++ src/ApiApprover/PublicApiApprovalTest.cs
@@ -1,24 +1,26 @@
-﻿using System;
-using ApiApprover;
-using ApprovalTests;
-using ApprovalTests.Reporters;
+﻿using System.Collections.Generic;
+using System.IO;
+using Xunit.Extensions;
 
 namespace ApiApprover
 {
-    public class PublicApiApprovalTest
+    public class ApiTests
     {
-        [Test]
-        [UseReporter(typeof(DiffReporter))] 
-        public void approve_public_api()
+        [Theory]
+        [PropertyData("AssemblyPaths")]
+        public void approve_public_api(string assembly, string path)
         {
-            // arrange
-            var assembly = typeof(SOMETYPE).Assembly;
-
-            // act
-            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(assembly);
-
-            // assert
-            Approvals.Verify(publicApi);
+            PublicApiApprover.ApprovePublicApi(Path.Combine(path, assembly), "results");
         }
-    }
-}
+
+        public static IEnumerable<object[]> AssemblyPaths
+        {
+            get
+            {
+                // Note that these are split to get nicer reporting in the test runner
+                // The assembly name comes first so we can read it, rather than the path
+                const string path = @"C:\temp";
+                yield return new object[] { "myAssembly.dll", path };
+            }
+        }
+    }}
---
+++ src/ApiApprover/PublicApiApprover.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.IO;
+using ApprovalTests;
+using ApprovalTests.Core;
+using ApprovalTests.Reporters;
+using Mono.Cecil;
+
+namespace ApiApprover
+{
+    public static class PublicApiApprover
+    {
+        public static void ApprovePublicApi(string assemblyPath, string resultsPath = "")
+        {
+            var sourcePath = Path.GetDirectoryName(new StackTrace(true).GetFrame(1).GetFileName()) ?? Path.GetTempPath();
+            sourcePath = Path.IsPathRooted(resultsPath) ? resultsPath : Path.Combine(sourcePath, resultsPath);
+            var asm = AssemblyDefinition.ReadAssembly(assemblyPath);
+            var publicApi = PublicApiGenerator.CreatePublicApiForAssembly(asm);
+            ApprovalTests.Approvals.Verify(new ApprovalTextWriter(publicApi), new AssemblyPathNamer(assemblyPath, sourcePath), new DiffReporter());
+        }
+
+        private class AssemblyPathNamer : IApprovalNamer
+        {
+            public AssemblyPathNamer(string assemblyPath, string sourcePath)
+            {
+                Name = Path.GetFileNameWithoutExtension(assemblyPath);
+                SourcePath = sourcePath;
+            }
+
+            public string SourcePath { get; private set; }
+            public string Name { get; private set; }
+        }
+    }
+}
