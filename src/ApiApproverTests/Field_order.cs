617faee62cf84e35faaaf8d9bb3cc130f9575d72
Matt Ellis <m.t.ellis@gmail.com>
2013-10-29 16:46:14 +0000

    Field order tests

---
+++ src/ApiApproverTests/Field_order.cs
@@ -0,0 +1,35 @@
+﻿using ApiApproverTests.Examples;
+using Xunit;
+
+namespace ApiApproverTests
+{
+    public class Field_order : ApiGeneratorTestsBase
+    {
+        [Fact]
+        public void Should_output_fields_in_alphabetical_order()
+        {
+            AssertPublicApi<FieldOrderExample>(
+@"namespace ApiApproverTests.Examples
+{
+    public class FieldOrderExample
+    {
+        public int AA_Field;
+        public string YY_Field;
+        public int ZZ_Field;
+    }
+}");
+        }
+    }
+
+    // ReSharper disable InconsistentNaming
+    namespace Examples
+    {
+        public class FieldOrderExample
+        {
+            public int ZZ_Field;
+            public string YY_Field;
+            public int AA_Field;
+        }
+    }
+    // ReSharper restore InconsistentNaming
+}
