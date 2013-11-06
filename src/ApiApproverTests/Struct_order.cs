33bd9245d4c6af969bf4d2fceba5ec0008ab96ba
Matt Ellis <m.t.ellis@gmail.com>
2013-11-06 15:19:12 +0000

    Struct ordering tests

---
+++ src/ApiApproverTests/Struct_order.cs
@@ -0,0 +1,43 @@
+﻿using ApiApproverTests.Examples;
+using Xunit;
+
+namespace ApiApproverTests
+{
+    public class Struct_order : ApiGeneratorTestsBase
+    {
+        [Fact]
+        public void Should_output_structs_in_alphabetical_order()
+        {
+            AssertPublicApi(new[] { typeof(ZZ_Struct), typeof(AA_Struct), typeof(MM_Struct) },
+@"namespace ApiApproverTests.Examples
+{
+    public struct AA_Struct { }
+}
+namespace ApiApproverTests.Examples
+{
+    public struct MM_Struct { }
+}
+namespace ApiApproverTests.Examples
+{
+    public struct ZZ_Struct { }
+}");
+        }
+    }
+
+    // ReSharper disable InconsistentNaming
+    namespace Examples
+    {
+        public struct ZZ_Struct
+        {
+        }
+
+        public struct AA_Struct
+        {
+        }
+
+        public struct MM_Struct
+        {
+        }
+    }
+    // ReSharper restore InconsistentNaming
+}
