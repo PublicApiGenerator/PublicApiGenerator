6882d4c138277f3e80c588c276a278c7ab30293b
Matt Ellis <m.t.ellis@gmail.com>
2014-02-10 16:19:00 +0000

    Assembly attributes

--- src/ApiApproverTests/ApiGeneratorTestsBase.cs
+++ src/ApiApproverTests/ApiGeneratorTestsBase.cs
@@ -12,26 +12,26 @@ namespace ApiApproverTests
     {
         private static readonly Regex StripEmptyLines = new Regex(@"^\s+$[\r\n]*", RegexOptions.Multiline | RegexOptions.Compiled);
 
-        protected void AssertPublicApi<T>(string expectedOutput)
+        protected void AssertPublicApi<T>(string expectedOutput, bool includeAssemblyAttributes = false)
         {
-            AssertPublicApi(typeof(T), expectedOutput);
+            AssertPublicApi(typeof(T), expectedOutput, includeAssemblyAttributes);
         }
 
-        protected void AssertPublicApi(Type type, string expectedOutput)
+        protected void AssertPublicApi(Type type, string expectedOutput, bool includeAssemblyAttributes = false)
         {
-            AssertPublicApi(new[] { type }, expectedOutput);
+            AssertPublicApi(new[] { type }, expectedOutput, includeAssemblyAttributes);
         }
 
-        protected void AssertPublicApi(Type[] types, string expectedOutput)
+        protected void AssertPublicApi(Type[] types, string expectedOutput, bool includeAssemblyAttributes = false)
         {
             var assemblyDefinition = AssemblyDefinition.ReadAssembly(GetType().Assembly.Location);
-            AssertPublicApi(assemblyDefinition, types, expectedOutput);
+            AssertPublicApi(assemblyDefinition, types, expectedOutput, includeAssemblyAttributes);
         }
 
-        private static void AssertPublicApi(AssemblyDefinition assemblyDefinition, Type[] types, string expectedOutput)
+        private static void AssertPublicApi(AssemblyDefinition assemblyDefinition, Type[] types, string expectedOutput, bool includeAssemblyAttributes)
         {
             var typesNames = new HashSet<string>(types.Select(t => t.FullName));
-            var actualOutput = PublicApiGenerator.CreatePublicApiForAssembly(assemblyDefinition, t => typesNames.Contains(t.FullName));
+            var actualOutput = PublicApiGenerator.CreatePublicApiForAssembly(assemblyDefinition, t => typesNames.Contains(t.FullName), includeAssemblyAttributes);
             actualOutput = StripEmptyLines.Replace(actualOutput, string.Empty);
             Assert.Equal(expectedOutput, actualOutput);
         }
--- src/ApiApproverTests/Properties/AssemblyInfo.cs
+++ src/ApiApproverTests/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -18,9 +17,6 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2209a69f-26ab-4494-8d1f-a8918c2ab82c")]
 
 // Version information for an assembly consists of the following four values:
 //
