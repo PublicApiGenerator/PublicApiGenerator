1221f6b81d272952e89b1998070f7462cb3a3524
Matt Ellis <m.t.ellis@gmail.com>
2014-02-11 15:29:05 +0000

    Interface inheritance tests

---
+++ src/ApiApproverTests/Interface_hierarchy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ApiApproverTests.Examples;
+using Xunit;
+
+namespace ApiApproverTests
+{
+    public class Interface_hierarchy : ApiGeneratorTestsBase
+    {
+        [Fact]
+        public void Should_output_implemented_interfaces_in_alphabetical_order()
+        {
+            AssertPublicApi<IInterfaceWithImplementedList>(
+@"namespace ApiApproverTests.Examples
+{
+    public interface IInterfaceWithImplementedList : System.ICloneable, System.IDisposable
+    {
+        void Method();
+    }
+}");
+        }
+    }
+
+    namespace Examples
+    {
+        public interface IInterfaceWithImplementedList : IDisposable, ICloneable
+        {
+            void Method();
+        }
+    }
+}
