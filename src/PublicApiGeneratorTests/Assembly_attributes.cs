34338760ed6ae11ee7731bd745b6d0f5d15ad7ed
danielmarbach <daniel.marbach@openplace.net>
2019-09-18 15:11:06 +0200

    Remove net40

--- src/PublicApiGeneratorTests/Assembly_attributes.cs
+++ src/PublicApiGeneratorTests/Assembly_attributes.cs
@@ -20,7 +20,6 @@ namespace PublicApiGeneratorTests
         [Fact]
         public void Attributes()
         {
-#if NET452
             var api = @"[assembly: PublicApiGeneratorTests.Examples.AttributeWithMultiplePositionalParametersAttribute(42, ""Hello"")]
 [assembly: PublicApiGeneratorTests.Examples.AttributeWithNamedParameterAttribute(IntValue=42, StringValue=""Hello"")]
 [assembly: PublicApiGeneratorTests.Examples.AttributeWithPositionalParameters1Attribute(""Hello"")]
@@ -28,7 +27,7 @@ namespace PublicApiGeneratorTests
 [assembly: PublicApiGeneratorTests.Examples.SimpleAttribute()]
 [assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
 [assembly: System.Runtime.InteropServices.GuidAttribute(""3B8D506A-5247-47FF-B053-D29A51A97C33"")]
-[assembly: System.Runtime.Versioning.TargetFrameworkAttribute("".NETFramework,Version=v4.5.2"", FrameworkDisplayName="".NET Framework 4.5.2"")]
+[assembly: System.Runtime.Versioning.TargetFrameworkAttribute("".NETCoreApp,Version=v2.2"", FrameworkDisplayName="""")]
 namespace PublicApiGeneratorTests.Examples
 {
     public class NotImportant
@@ -36,25 +35,6 @@ namespace PublicApiGeneratorTests.Examples
         public NotImportant() { }
     }
 }";
-#endif
-
-#if NETCOREAPP2_1
-            var api = @"[assembly: PublicApiGeneratorTests.Examples.AttributeWithMultiplePositionalParametersAttribute(42, ""Hello"")]
-[assembly: PublicApiGeneratorTests.Examples.AttributeWithNamedParameterAttribute(IntValue=42, StringValue=""Hello"")]
-[assembly: PublicApiGeneratorTests.Examples.AttributeWithPositionalParameters1Attribute(""Hello"")]
-[assembly: PublicApiGeneratorTests.Examples.AttributeWithPositionalParameters2Attribute(42)]
-[assembly: PublicApiGeneratorTests.Examples.SimpleAttribute()]
-[assembly: System.Runtime.InteropServices.ComVisibleAttribute(false)]
-[assembly: System.Runtime.InteropServices.GuidAttribute(""3B8D506A-5247-47FF-B053-D29A51A97C33"")]
-[assembly: System.Runtime.Versioning.TargetFrameworkAttribute("".NETCoreApp,Version=v2.1"", FrameworkDisplayName="""")]
-namespace PublicApiGeneratorTests.Examples
-{
-    public class NotImportant
-    {
-        public NotImportant() { }
-    }
-}";
-#endif
 
             AssertPublicApi<NotImportant>(api, true);
         }
