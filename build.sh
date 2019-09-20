4a2c0fa12434811762dcfd04cb80c8ef38da2992
danielmarbach <daniel.marbach@openplace.net>
2019-09-20 10:14:44 +0200

    See if this works

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+set -euo pipefail
+
 dotnet build ./src/ApiApprover.sln --configuration Release
 dotnet test ./src/ApiApprover.sln --configuration Release --no-build --verbosity=normal
 dotnet publish ./src/PublicApiGenerator/PublicApiGenerator.csproj --configuration Release --no-build /nologo
