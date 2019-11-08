0a4cc349a590bf68ffde2f6255d21a597e0a59e4
danielmarbach <daniel.marbach@openplace.net>
2019-11-08 21:41:27 +0100

    Refactor EventBuilderName and support new abstract

--- src/PublicApiGenerator/EventNameBuilder.cs
+++ src/PublicApiGenerator/EventNameBuilder.cs
@@ -1,4 +1,6 @@
+using System;
 using System.CodeDom;
+using System.Linq;
 using Mono.Cecil;
 
 namespace PublicApiGenerator
@@ -8,52 +10,51 @@ namespace PublicApiGenerator
         public static string AugmentEventNameWithEventModifierMarkerTemplate(EventDefinition eventDefinition, string name,
             MemberAttributes addAccessorAttributes, MemberAttributes removeAccessorAttributes)
         {
-            var addVTableAttributes = (addAccessorAttributes & MemberAttributes.VTableMask);
-            var removeVTableAttributes = (removeAccessorAttributes & MemberAttributes.VTableMask);
+            if (addAccessorAttributes != removeAccessorAttributes)
+            {
+                return name;
+            }
 
             if (eventDefinition.DeclaringType.IsInterface)
             {
-                if (addVTableAttributes == MemberAttributes.New && removeVTableAttributes == MemberAttributes.New)
+                return (addAccessorAttributes & MemberAttributes.VTableMask) == MemberAttributes.New
+                    ? string.Format(CodeNormalizer.EventModifierMarkerTemplate, $"new{CodeNormalizer.EventRemovePublicMarker}") + name
+                    : string.Format(CodeNormalizer.EventModifierMarkerTemplate, CodeNormalizer.EventRemovePublicMarker) + name;
+            }
+
+            bool? isNew = null;
+            var baseType = eventDefinition.DeclaringType.BaseType;
+            while (baseType != null)
+            {
+                var typeDef = baseType as TypeDefinition;
+                isNew = typeDef?.Methods.Any(e => e.Name.Equals(eventDefinition.AddMethod.Name, StringComparison.Ordinal));
+                if (isNew.HasValue && isNew.Value)
                 {
-                    return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, $"new{CodeNormalizer.EventRemovePublicMarker}")}{name}";
+                    break;
                 }
-
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, CodeNormalizer.EventRemovePublicMarker)}{name}";;
+                baseType = typeDef?.BaseType;
             }
 
-            var addScopeAttributes = (addAccessorAttributes & MemberAttributes.ScopeMask);
-            var removeScopeAttributes = (removeAccessorAttributes & MemberAttributes.ScopeMask);
-
-            if (addScopeAttributes == MemberAttributes.Static && removeScopeAttributes == MemberAttributes.Static)
+            var addScopeAttributes = addAccessorAttributes & MemberAttributes.ScopeMask;
+            switch (addScopeAttributes)
             {
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, "static")}{name}";
+                case MemberAttributes.Static when addScopeAttributes == MemberAttributes.Static:
+                    return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "static")+ name;
+                case MemberAttributes.Override when addScopeAttributes == MemberAttributes.Override:
+                    return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "override")+ name;
+                case MemberAttributes.Final | MemberAttributes.Override when addScopeAttributes == (MemberAttributes.Final | MemberAttributes.Override):
+                    return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "sealed override")+ name;
+                case MemberAttributes.Final when addScopeAttributes == MemberAttributes.Final && isNew.HasValue && isNew.Value:
+                    return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "new")+ name;
+                case MemberAttributes.Abstract when addScopeAttributes == MemberAttributes.Abstract && !isNew.HasValue:
+                    return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "abstract")+ name;
+                case MemberAttributes.Abstract when addScopeAttributes == MemberAttributes.Abstract && isNew.HasValue && isNew.Value:
+                    return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "new abstract")+ name;
             }
 
-
-            if (addScopeAttributes == MemberAttributes.Override && removeScopeAttributes == MemberAttributes.Override)
+            if (eventDefinition.AddMethod.IsVirtual && !eventDefinition.AddMethod.IsAbstract)
             {
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, "override")}{name}";
-            }
-
-            if (addScopeAttributes == (MemberAttributes.Final | MemberAttributes.Override) && removeScopeAttributes == (MemberAttributes.Final | MemberAttributes.Override))
-            {
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, "sealed override")}{name}";
-            }
-
-            if (addScopeAttributes == MemberAttributes.Abstract && removeScopeAttributes == MemberAttributes.Abstract)
-            {
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, "abstract")}{name}";
-            }
-
-            if (addVTableAttributes == MemberAttributes.New && removeVTableAttributes == MemberAttributes.New)
-            {
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, "new")}{name}";
-            }
-
-            if (eventDefinition.AddMethod.IsVirtual && eventDefinition.RemoveMethod.IsVirtual &&
-                !eventDefinition.AddMethod.IsAbstract && !eventDefinition.RemoveMethod.IsAbstract)
-            {
-                return $"{string.Format(CodeNormalizer.EventModifierMarkerTemplate, "virtual")}{name}";
+                return string.Format(CodeNormalizer.EventModifierMarkerTemplate, "virtual")+ name;
             }
 
             return name;
