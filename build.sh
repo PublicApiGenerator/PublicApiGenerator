a588bc1e8f255477bb05c6110a3ec9a758528482
Adam Ralph <adam@adamralph.com>
2019-11-12 15:29:13 +0200

    align build.cmd and build.sh more

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
 
-dotnet build ./src/ApiApprover.sln --configuration Release
-dotnet test ./src/ApiApprover.sln --configuration Release --no-build --verbosity=normal
+dotnet build ./src/ApiApprover.sln --configuration Release /nologo
+dotnet test ./src/ApiApprover.sln --configuration Release --no-build --verbosity=normal /nologo
 dotnet publish ./src/PublicApiGenerator/PublicApiGenerator.csproj --configuration Release --no-build /nologo
 dotnet pack ./src/PublicApiGenerator/PublicApiGenerator.csproj --configuration Release --no-build /nologo
 dotnet publish ./src/PublicApiGenerator.Tool/PublicApiGenerator.Tool.csproj --configuration Release --no-build /nologo
