6e1e41175990711eee8873753cbc0b138fc87434
Ivan Maximov <sungam3r@yandex.ru>
2022-12-05 10:48:42 +0300

    Update CI and project properties

--- src/PublicApiGeneratorTests/Assembly_attributes.cs
+++ src/PublicApiGeneratorTests/Assembly_attributes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Runtime.InteropServices;
 using PublicApiGenerator;
 using PublicApiGeneratorTests.Examples;
@@ -21,21 +21,31 @@ namespace PublicApiGeneratorTests
         [Fact]
         public void Attributes()
         {
-            var api = @"[assembly: PublicApiGeneratorTests.Examples.AttributeWithMultiplePositionalParameters(42, ""Hello"")]
+#if NET7_0
+            const string TFM = ".NETCoreApp,Version=v7.0";
+            const string TFMNAME = ".NET 7.0";
+#elif NET6_0
+            const string TFM = ".NETCoreApp,Version=v6.0";
+            const string TFMNAME = ".NET 6.0";
+#else
+            const string TFM = ".NETCoreApp,Version=v3.1";
+            const string TFMNAME = ".NET Core 3.1";
+#endif
+            var api = @$"[assembly: PublicApiGeneratorTests.Examples.AttributeWithMultiplePositionalParameters(42, ""Hello"")]
 [assembly: PublicApiGeneratorTests.Examples.AttributeWithNamedParameter(IntValue=42, StringValue=""Hello"")]
 [assembly: PublicApiGeneratorTests.Examples.AttributeWithPositionalParameters1(""Hello"")]
 [assembly: PublicApiGeneratorTests.Examples.AttributeWithPositionalParameters2(42)]
 [assembly: PublicApiGeneratorTests.Examples.Simple]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid(""3B8D506A-5247-47FF-B053-D29A51A97C33"")]
-[assembly: System.Runtime.Versioning.TargetFramework("".NETCoreApp,Version=v3.1"", FrameworkDisplayName="""")]
+[assembly: System.Runtime.Versioning.TargetFramework(""{TFM}"", FrameworkDisplayName=""{TFMNAME}"")]
 namespace PublicApiGeneratorTests.Examples
-{
+{{
     public class NotImportant
-    {
-        public NotImportant() { }
-    }
-}";
+    {{
+        public NotImportant() {{ }}
+    }}
+}}";
 
             AssertPublicApi<NotImportant>(api, new ApiGeneratorOptions { IncludeAssemblyAttributes = true });
         }
