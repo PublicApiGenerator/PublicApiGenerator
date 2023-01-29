190576ee7395a6bc7b7627c6a179bdbb1156a35e
Ivan Maximov <sungam3r@yandex.ru>
2023-01-29 16:31:06 +0300

    Update CI and project properties (#255)

--- build.sh
+++
@@ -1,8 +0,0 @@
-set -euo pipefail
-
-dotnet build ./src/PublicApiGenerator.sln --configuration Release /nologo
-dotnet test ./src/PublicApiGenerator.sln --configuration Release --no-build --verbosity=normal /nologo
-dotnet publish ./src/PublicApiGenerator/PublicApiGenerator.csproj --configuration Release --no-build /nologo
-dotnet pack ./src/PublicApiGenerator/PublicApiGenerator.csproj --configuration Release --no-build /nologo
-dotnet publish ./src/PublicApiGenerator.Tool/PublicApiGenerator.Tool.csproj --configuration Release --no-build /nologo
-dotnet pack ./src/PublicApiGenerator.Tool/PublicApiGenerator.Tool.csproj --configuration Release --no-build /nologo
--- src/PublicApiGeneratorTests/Assembly_attributes.cs
+++ src/PublicApiGeneratorTests/Assembly_attributes.cs
@@ -21,10 +21,15 @@ namespace PublicApiGeneratorTests
         [Fact]
         public void Attributes()
         {
-#if NET6_0
+#if NET7_0
+            const string TFM = ".NETCoreApp,Version=v7.0";
+            const string TFMNAME = ".NET 7.0";
+#elif NET6_0
             const string TFM = ".NETCoreApp,Version=v6.0";
+            const string TFMNAME = ".NET 6.0";
 #else
             const string TFM = ".NETCoreApp,Version=v3.1";
+            const string TFMNAME = ".NET Core 3.1";
 #endif
             var api = @$"[assembly: PublicApiGeneratorTests.Examples.AttributeWithMultiplePositionalParameters(42, ""Hello"")]
 [assembly: PublicApiGeneratorTests.Examples.AttributeWithNamedParameter(IntValue=42, StringValue=""Hello"")]
@@ -33,7 +38,7 @@ namespace PublicApiGeneratorTests
 [assembly: PublicApiGeneratorTests.Examples.Simple]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid(""3B8D506A-5247-47FF-B053-D29A51A97C33"")]
-[assembly: System.Runtime.Versioning.TargetFramework(""{TFM}"", FrameworkDisplayName="""")]
+[assembly: System.Runtime.Versioning.TargetFramework(""{TFM}"", FrameworkDisplayName=""{TFMNAME}"")]
 namespace PublicApiGeneratorTests.Examples
 {{
     public class NotImportant
