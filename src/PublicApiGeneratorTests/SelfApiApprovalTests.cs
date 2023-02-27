b215429590087d7610a512ef165001914ecb6128
Ivan Maximov <sungam3r@yandex.ru>
2023-02-27 14:35:53 +0300

    Add API approval test

---
+++ src/PublicApiGeneratorTests/SelfApiApprovalTests.cs
@@ -0,0 +1,21 @@
+using PublicApiGenerator;
+using Shouldly;
+
+namespace PublicApiGeneratorTests;
+
+public class SelfApiApprovalTests
+{
+    [Fact]
+    public void PublicApi_Should_Not_Change_Unintentionally()
+    {
+        var assembly = typeof(ApiGeneratorOptions).Assembly;
+        var publicApi = assembly.GeneratePublicApi(
+            new()
+            {
+                IncludeAssemblyAttributes = false,
+                ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" },
+            });
+
+        publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((_, _, fileType, fileExtension) => $"{assembly.GetName().Name!}.{fileType}.{fileExtension}"));
+    }
+}
