1c49f139f229b75c924dfc30191c7d0bfbfe41df
Ivan Maximov <sungam3r@yandex.ru>
2023-02-28 13:09:48 +0300

    Introduce two new options

--- src/PublicApiGenerator/ApiGeneratorOptions.cs
+++ src/PublicApiGenerator/ApiGeneratorOptions.cs
@@ -1,33 +1,59 @@
 namespace PublicApiGenerator;
 
 /// <summary>
-/// Options to influence the ApiGenerator
+/// Options to influence the ApiGenerator output.
 /// </summary>
 public class ApiGeneratorOptions
 {
     /// <summary>
-    /// Allows to control which types of the generated assembly should be included. If this option is specified all other types found in the assembly that are not present here will be exclude.
+    /// Allows to control which types of the generated assembly should be included.
+    /// If this option is specified all other types found in the assembly that are not present here will be excluded.
     /// </summary>
     public Type[]? IncludeTypes { get; set; }
 
     /// <summary>
     /// Instructs the generator to include assembly level attributes.
     /// </summary>
-    /// <remarks>Defaults to true</remarks>
+    /// <remarks>Defaults to <see langword="true"/>.</remarks>
     public bool IncludeAssemblyAttributes { get; set; } = true;
 
     /// <summary>
-    /// Allows to whitelist certain namespace prefixes. For example by default types found in Microsoft or System namespaces are not treated as part of the public API.
+    /// Allows to whitelist certain namespace prefixes.
+    /// For example by default types found in Microsoft or System namespaces are not treated as part of the public API.
+    /// This option has priority over <see cref="BlacklistedNamespacePrefixes"/>.
     /// </summary>
     /// <example>
     /// <code>
     /// var options = new DefaultApiGeneratorOptions
     /// {
-    ///    WhitelistedNamespacePrefixes = new[] {"Microsoft.Whitelisted" }
+    ///    WhitelistedNamespacePrefixes = new[] { "Microsoft.Whitelisted" }
     /// };
     /// </code>
     /// </example>
-    public string[] WhitelistedNamespacePrefixes { get; set; } = DefaultWhitelistedNamespacePrefixes;
+    public string[] WhitelistedNamespacePrefixes { get; set; } = _defaultWhitelistedNamespacePrefixes;
+
+    /// <summary>
+    /// Allows to blacklist certain namespace prefixes.
+    /// By default types found in Microsoft or System namespaces are not treated as part of the public API.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// var options = new DefaultApiGeneratorOptions
+    /// {
+    ///    BlacklistedNamespacePrefixes = new[] { "System", "Microsoft", "ThirdParty" }
+    /// };
+    /// </code>
+    /// </example>
+    public string[] BlacklistedNamespacePrefixes { get; set; } = _defaultBlacklistedNamespacePrefixes;
+
+    /// <summary>
+    /// Allows to control whether to include extension methods into generated API even when
+    /// containing class falls into <see cref="BlacklistedNamespacePrefixes"/> option. This
+    /// option may be useful, for example, for those who writes extensions for IServiceCollection
+    /// keeping them into Microsoft.Extensions.DependencyInjection namespace for better discoverability.
+    /// </summary>
+    /// <remarks>Defaults to <see langword="true"/>, i.e. extension methods are excluded from output.</remarks>
+    public bool UseBlacklistedNamespacePrefixesForExtensionMethods { get; set; } = true;
 
     /// <summary>
     /// Allows to exclude attributes by specifying the fullname of the attribute to exclude.
@@ -42,5 +68,7 @@ public class ApiGeneratorOptions
     /// </example>
     public string[]? ExcludeAttributes { get; set; }
 
-    static readonly string[] DefaultWhitelistedNamespacePrefixes = Array.Empty<string>();
+    private static readonly string[] _defaultWhitelistedNamespacePrefixes = Array.Empty<string>();
+
+    private static readonly string[] _defaultBlacklistedNamespacePrefixes = new[] { "System", "Microsoft" };
 }
