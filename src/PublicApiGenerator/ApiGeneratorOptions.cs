0cef8cccd7be494e789e6a1021bf67eadfeadfca
Ivan Maximov <sungam3r@yandex.ru>
2024-12-09 19:40:47 +0300

    Update src/PublicApiGenerator/ApiGeneratorOptions.cs

--- src/PublicApiGenerator/ApiGeneratorOptions.cs
+++ src/PublicApiGenerator/ApiGeneratorOptions.cs
@@ -92,9 +92,9 @@ public class ApiGeneratorOptions
         get => TypeComparer is FullNameComparer ? OrderMode.FullName : OrderMode.NamespaceThenFullName;
         set
         {
-            TypeComparer = value == OrderMode.NamespaceThenFullName
-                ? new NamespaceThenFullNameComparer()
-                : new FullNameComparer();
+            TypeComparer = value == OrderMode.FullName
+                ? new FullNameComparer()
+                : new NamespaceThenFullNameComparer();
         }
     }
 
