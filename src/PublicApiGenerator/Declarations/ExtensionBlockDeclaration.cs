854c2fbe1398496df7d3f1eef774a0f69923cfcd
Stuart Lang <stuart.b.lang@gmail.com>
2025-11-25 22:57:32 +0000

    Fix extensions bug (#491)
    * Fix extensions bug
    
    * Update src/PublicApiGenerator/Declarations/ExtensionBlockDeclaration.cs
    
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>
    
    ---------
    
    Co-authored-by: Ivan Maximov <sungam3r@yandex.ru>

--- src/PublicApiGenerator/Declarations/ExtensionBlockDeclaration.cs
+++ src/PublicApiGenerator/Declarations/ExtensionBlockDeclaration.cs
@@ -13,7 +13,15 @@ internal class ExtensionBlockDeclaration : CodeTypeDeclaration
             Name = AnchorMethod.Parameters[0].Name;
         AnchorType = AnchorMethod.Parameters[0].ParameterType.CreateCodeTypeReference();
         // rename $T0 $T1 in extension blocks to TKey TValue taking generic parameter names from this anchor
-        Rewrite = x => x.StartsWith("$T") ? ((GenericInstanceType)AnchorMethod.Parameters[0].ParameterType).GenericArguments[int.Parse(x.Substring(2))].Name : x;
+        Rewrite = x => x.StartsWith("$T")
+            ? AnchorMethod.Parameters[0].ParameterType switch
+            {
+                GenericInstanceType genericInstanceType => genericInstanceType
+                    .GenericArguments[int.Parse(x.Substring(2))].Name,
+                GenericParameter genericParameter => genericParameter.Name,
+                object unknown => throw new NotSupportedException($"Unknown parameter type {unknown}.")
+            }
+            : x;
     }
 
     public MethodDefinition AnchorMethod { get; }
